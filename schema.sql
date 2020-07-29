045c61c08aa870082a643d3d521ea98aba63b132
sushi2k <sven@bsddaemon.org>
2020-07-29 19:46:20 +0800

    removed create database

--- schema.sql
+++ schema.sql
@@ -1,4 +1,3 @@
-CREATE DATABASE `feinstaub` /!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /!80016 DEFAULT ENCRYPTION='N' */;
 CREATE TABLE `sensordaten` (
   `PM25` decimal(4,2) DEFAULT NULL,
   `PM10` decimal(4,2) DEFAULT NULL,
