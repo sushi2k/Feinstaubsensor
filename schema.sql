4983a7223e0af94af5c350e128d0c1d34a951a6e
sushi2k <sven@bsddaemon.org>
2020-07-29 20:08:24 +0800

    remove meta-data

--- schema.sql
+++ schema.sql
@@ -6,4 +6,4 @@ CREATE TABLE `sensordaten` (
   `Humidity` decimal(4,2) DEFAULT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=78 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB;
